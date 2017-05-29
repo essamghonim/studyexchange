2ddc16a692c6f5c0abbdf0ab4163803c8bc5338c
essamghonim <ee14eno@my.brunel.ac.uk>
2017-05-29 14:27:55 +0100

    Update index.js

--- index.js
+++ index.js
@@ -21,7 +21,7 @@ if (process.env.APNS_ENABLE) {
     pushConfig['ios'] = [
         {
             pfx: __dirname + 'StudyBuddiesCertificates.p12', // P12 file only
-            cert: __dirname + 'StudyBuddiesck.pem', // If not using the .p12 format, the path to the certificate PEM to load from disk
+            cert: __dirname + 'StudyBuddiesCert.pem', // If not using the .p12 format, the path to the certificate PEM to load from disk
             key: __dirname + 'StudyBuddiesKey.unencrypted.pem', // If not using the .p12 format
             bundleId: 'Connect.Ltd.StudyExchange',  // change to match bundleId
             production: false // dev certificate
